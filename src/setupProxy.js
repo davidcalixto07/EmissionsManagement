c5bafa2b60845f96bc66639aeb73c3a2234ba152
Yexxd <91701092+Yexxd@users.noreply.github.com>
2024-04-11 09:18:10 -0500

    push

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -8,14 +8,17 @@ module.exports = function (app) {
       pathRewrite: {
         '^/api/emissionsapi2-colwest2/v1/': '', // Cut the path to redirect to the endpoint
       },
-      changeOrigin: true, 
+      changeOrigin: true,
     })
   );
 
   app.use(
-    '/api',
+    '/api/',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:5001',
+      target: 'http://127.0.0.1:5500',
+      pathRewrite: {
+        '^/api/': '', // Cut the path to redirect to the endpoint
+      }
     })
   );
 };
