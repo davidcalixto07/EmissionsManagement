5151069410566cd7cecfc8120a92e11fca09e275
Yexxd <91701092+Yexxd@users.noreply.github.com>
2024-04-12 12:41:36 -0500

    proxyFix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,18 +2,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    '/api/emissionsapi2-colwest2/v1/',
-    createProxyMiddleware({
-      target: 'http://127.0.0.1:9099',
-      pathRewrite: {
-        '^/api/emissionsapi2-colwest2/v1/': '', // Cut the path to redirect to the endpoint
-      },
-      changeOrigin: true,
-      changeOrigin: true,
-    })
-  );
-
-  app.use(
     '/api/',
     createProxyMiddleware({
       target: 'http://127.0.0.1:5500',
